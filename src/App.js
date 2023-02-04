d182289ff35215ef4436d1d66f032c4397255675
Matheus <matheuspl92@hotmail.com>
2023-02-04 15:09:11 -0300

    Add Form component and refactor code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Form from './components/Form';
+import Overview from './components/Overview';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { tasks: [] };
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        <Overview tasks={this.state.tasks} />
+        <Form/>
+      </div>
     )
   }
 }
